1d82b3879392c39fb1eb35513656cb09dbd17f64
jmeegan2 <jmeegan8@gmail.com>
2023-04-02 18:57:35 -0400

    calendar popup added, AddScreen()

--- app/src/main/java/edu/towson/cosc435/meegan/semesterprojectpantrypal/AddScreen.kt
+++ app/src/main/java/edu/towson/cosc435/meegan/semesterprojectpantrypal/AddScreen.kt
@@ -1,7 +1,10 @@
 package edu.towson.cosc435.meegan.semesterprojectpantrypal
 
+import android.app.DatePickerDialog
+import android.widget.DatePicker
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
@@ -18,98 +22,122 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
-
+import java.util.*
 @Composable
 fun AddScreen() {
 
+    //For Date
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+    var selectedDateText by remember { mutableStateOf("") }
+    val year = calendar[Calendar.YEAR]
+    val month = calendar[Calendar.MONTH]
+    val dayOfMonth = calendar[Calendar.DAY_OF_MONTH]
 
-    // Initialize a mutable list of items from the App State
+    //For InputFields
     val items = remember { AppState.items.toMutableList() }
+    val itemNameState = remember { mutableStateOf(TextFieldValue("")) }
+    val categoryState = remember { mutableStateOf(TextFieldValue("")) }
+    val quantityState = remember { mutableStateOf(TextFieldValue("")) }
+    val expirationDateState = remember { mutableStateOf(TextFieldValue("")) }
 
-    // Initialize a list of fields that users can fill in to add a new item
-    val fields = listOf(
-        "Item name" to remember { mutableStateOf(TextFieldValue("")) },
-        "Category" to remember { mutableStateOf(TextFieldValue("")) },
-        "Quantity" to remember { mutableStateOf(TextFieldValue("")) },
-        "Expiration Date (YYYY-MM-DD)" to remember { mutableStateOf(TextFieldValue("")) }
+    //Choosing of Date
+    val datePicker = DatePickerDialog(
+        context,
+        { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDayOfMonth: Int ->
+            selectedDateText = "${selectedMonth + 1}-$selectedDayOfMonth-$selectedYear"
+            expirationDateState.value = TextFieldValue(selectedDateText)
+        }, year, month, dayOfMonth
     )
 
-    // Initialize mutable state variables for the confirmation message and whether to show it
+    //Can't choose past dates
+    datePicker.datePicker.minDate = calendar.timeInMillis
+
+    //Item added confirmation
     val confirmationMessage = remember { mutableStateOf("") }
     val showMessage = remember { mutableStateOf(false) }
 
-    // Define a composable function for the input fields
     @Composable
     fun InputField(
         label: String,
         state: MutableState<TextFieldValue>,
         modifier: Modifier = Modifier,
-        keyboardOptions: KeyboardOptions = KeyboardOptions.Default
+        keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+        onlyNum: Boolean = false,
+        onlyLet: Boolean = false,
+        onClick: (() -> Unit)? = null
     ) {
-        TextField(
-            value = state.value,
-            onValueChange = { state.value = it },
-            label = { Text(label) },
-            modifier = modifier,
-            keyboardOptions = keyboardOptions
-        )
+        Box(
+            modifier = modifier.clickable(onClick = {
+                onClick?.invoke()
+            })
+        ) {
+            TextField(
+                value = state.value,
+                onValueChange = { newValue ->
+                    if (onlyNum && newValue.text.any { !it.isDigit() }) return@TextField
+                    if (onlyLet && newValue.text.any { !it.isLetterOrDigit() && it != ' ' }) return@TextField
+                    state.value = newValue
+                },
+                label = { Text(label) },
+                modifier = modifier,
+                keyboardOptions = keyboardOptions,
+                enabled = onClick == null // Disable TextField if onClick is provided
+            )
+        }
 
         Spacer(modifier = Modifier.height(8.dp))
     }
 
-    // Define the layout for the add screen
-    Column(
 
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background("#e3ffde".toColor())
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-//        Spacer(modifier = Modifier.height(16.dp))
+//        Button(onClick = { datePicker.show() }) { Text(text = "Select a date") }
 
         Button(
-            onClick = {
-                // Handle barcode scanner functionality here
-            },
+            onClick = { /* Handle barcode scanner functionality here */ },
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .fillMaxWidth(0.80f),
             colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray)
-        ) {
-            Text("Barcode Scanner", color = Color.Black, fontSize = 19.sp)
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        // Display the input fields
-        fields.forEach { (label, state) ->
-            val keyboardType =
-                if (label == "Quantity" || label == "Expiration Date (MM-DD-YYYY)") KeyboardType.Number else KeyboardType.Text
-            InputField(label, state, keyboardOptions = KeyboardOptions(keyboardType = keyboardType))
-        }
+        ) { Text("Barcode Scanner", color = Color.Black, fontSize = 19.sp) }
 
-        // Display a button to confirm the item and add it to the list
+        Spacer(modifier = Modifier.height(16.dp))
+
+        InputField("Item name", itemNameState, onlyLet = true)
+        InputField("Category", categoryState, onlyLet = true)
+        InputField("Quantity", quantityState, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number), onlyNum = true)
+        InputField(
+            "Expiration Date (MM-DD-YYYY)",
+            expirationDateState,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onClick = {
+                datePicker.show()
+            }
+        )
         Button(onClick = {
-            // Create a new item from the input fields and add it to the list of items
             val newItem = Item(
-                fields[0].second.value.text,
-                fields[1].second.value.text,
-                fields[2].second.value.text,
-                fields[3].second.value.text
+                itemNameState.value.text,
+                categoryState.value.text,
+                quantityState.value.text,
+                expirationDateState.value.text
             )
             items.add(newItem)
-
-            // Update the App State with the new list of items
             AppState.items = items.toList()
-
-            // Reset the input fields to blank
-            fields.forEach { (_, state) -> state.value = TextFieldValue("") }
-
-            // Update the confirmation message and show it
             confirmationMessage.value = "Item added to inventory"
             showMessage.value = true
-        }) {
-            Text("Confirm")
-        }
+
+            // Clear the input fields
+            itemNameState.value = TextFieldValue("")
+            categoryState.value = TextFieldValue("")
+            quantityState.value = TextFieldValue("")
+            expirationDateState.value = TextFieldValue("")
+        }) { Text("Confirm") }
 
         Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
             if (items.isNotEmpty()) {
@@ -121,8 +149,6 @@ fun AddScreen() {
             }
         }
 
-
-        // Display the list of items in a LazyColumn
         Spacer(modifier = Modifier.height(4.dp))
         LazyColumn {
             items.forEach { item ->

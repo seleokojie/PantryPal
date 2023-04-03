c755c9df6e8419079e5b34dd40d60dd123d236fb
jmeegan2 <jmeegan8@gmail.com>
2023-04-02 20:24:05 -0400

    just been messing with formatting of add screen for awhile

--- app/src/main/java/edu/towson/cosc435/meegan/semesterprojectpantrypal/AddScreen.kt
+++ app/src/main/java/edu/towson/cosc435/meegan/semesterprojectpantrypal/AddScreen.kt
@@ -41,7 +41,7 @@ fun AddScreen() {
     val quantityState = remember { mutableStateOf(TextFieldValue("")) }
     val expirationDateState = remember { mutableStateOf(TextFieldValue("")) }
     val errorState = remember { mutableStateOf(false) }
-
+    val categories = listOf("Fruit", "Vegetable", "Meat", "Dairy", "Bakery")
     //Choosing of Date
     val datePicker = DatePickerDialog(
         context,
@@ -66,29 +66,80 @@ fun AddScreen() {
         keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
         onlyNum: Boolean = false,
         onlyLet: Boolean = false,
+        categories: List<String> = emptyList(),
         onClick: (() -> Unit)? = null
     ) {
-        Box(
-            modifier = modifier.clickable(onClick = {
-                onClick?.invoke()
-            })
-        ) {
-            TextField(
-                value = state.value,
-                onValueChange = { newValue ->
-                    if (onlyNum && newValue.text.any { !it.isDigit() }) return@TextField
-                    if (onlyLet && newValue.text.any { !it.isLetter() && it != ' ' }) return@TextField
-                    state.value = newValue
-                },
-                label = { Text(label) },
-                modifier = modifier,
-                keyboardOptions = keyboardOptions,
-                enabled = onClick == null // Disable TextField if onClick is provided
-            )
-        }
+        var selectedIndex by remember { mutableStateOf(-1) }
 
+        Column(modifier) {
+            if (categories.isNotEmpty()) {
+                var expanded by remember { mutableStateOf(false) }
+
+                Box(Modifier.clickable(onClick = { expanded = true })) {
+                    TextField(
+                        value = if (selectedIndex >= 0) state.value else TextFieldValue(""),
+                        onValueChange = { newValue ->
+                            if (onlyNum && newValue.text.any { !it.isDigit() }) return@TextField
+                            if (onlyLet && newValue.text.any { !it.isLetter() && it != ' ' }) return@TextField
+                            state.value = newValue
+                        },
+                        label = { Text(label) },
+                        modifier = Modifier.fillMaxWidth(0.7f),
+                        keyboardOptions = keyboardOptions,
+                        enabled = false
+                    )
+                }
+
+                DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                    categories.forEachIndexed { index, category ->
+                        DropdownMenuItem(onClick = {
+                            selectedIndex = index
+                            state.value = TextFieldValue(category)
+                            expanded = false
+                        }) {
+                            Text(text = category)
+                        }
+                    }
+                }
+            } else {
+                if (onClick != null) {
+                    Box(Modifier.clickable(onClick = onClick).fillMaxWidth(0.7f)) {
+                        TextField(
+                            value = state.value,
+                            onValueChange = { newValue ->
+                                if (onlyNum && newValue.text.any { !it.isDigit() }) return@TextField
+                                if (onlyLet && newValue.text.any { !it.isLetter() && it != ' ' }) return@TextField
+                                state.value = newValue
+                            },
+                            label = { Text(label) },
+                            modifier = Modifier.fillMaxWidth(),
+                            keyboardOptions = keyboardOptions,
+                            enabled = false
+                        )
+                    }
+                } else {
+                    TextField(
+                        value = state.value,
+                        onValueChange = { newValue ->
+                            if (onlyNum && newValue.text.any { !it.isDigit() }) return@TextField
+                            if (onlyLet && newValue.text.any { !it.isLetter() && it != ' ' }) return@TextField
+                            state.value = newValue
+                        },
+                        label = { Text(label) },
+                        modifier = Modifier.fillMaxWidth(0.7f),
+                        keyboardOptions = keyboardOptions
+                    )
+                }
+            }
+        }
         Spacer(modifier = Modifier.height(8.dp))
     }
+
+
+
+
+
+
 
 
     Column(
@@ -98,20 +149,20 @@ fun AddScreen() {
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-//        Button(onClick = { datePicker.show() }) { Text(text = "Select a date") }
-
         Button(
             onClick = { /* Handle barcode scanner functionality here */ },
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .fillMaxWidth(0.80f),
             colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray)
-        ) { Text("Barcode Scanner", color = Color.Black, fontSize = 19.sp) }
+        ) {
+            Text("Barcode Scanner", color = Color.Black, fontSize = 19.sp)
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         InputField("Item name", itemNameState, onlyLet = true)
-        InputField("Category", categoryState, onlyLet = true)
+        InputField("Category", categoryState, categories = categories, onlyLet = true)
         InputField("Quantity", quantityState, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number), onlyNum = true)
         InputField(
             "Expiration Date (MM-DD-YYYY)",
